2f16e99ffea80828f06b985030de96cc35f68a66
Nathi2266 <Nathiradebe71@gmail.com>
2026-05-12 11:36:36 +0200

    Feature: fixed the login bug.

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
 import '../providers/auth_provider.dart';
